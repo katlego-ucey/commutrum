b6e15aab5420898d509af8d31a17d74d1a3cdbb8
katlego-ucey <ramokgadi.k@proton.me>
2026-06-30 09:14:34 +0200

    Delete lib directory

--- lib/db/src/schema/index.ts
+++
@@ -1,6 +0,0 @@
-export * from "./factorDefinitions";
-export * from "./hypothesisRegistry";
-export * from "./compositeScores";
-export * from "./portfolioTargets";
-export * from "./monitoringMetrics";
-export * from "./monitoringAlerts";
